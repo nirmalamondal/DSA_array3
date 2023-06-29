e155f031faa2cf64431abb6d24f155468596f1fb
nirmalamondal <121811026+nirmalamondal@users.noreply.github.com>
2023-06-29 11:49:31 +0530

    Update arrange_coins.cpp

--- arrange_coins.cpp
+++ arrange_coins.cpp
@@ -1,7 +1,7 @@
-'''**Question 5**
+/* Question 5**
 You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase **may be** incomplete.
 
-Given the integer n, return *the number of **complete rows** of the staircase you will build*.'''
+Given the integer n, return *the number of **complete rows** of the staircase you will build*.*/
 
 #include<bits/stdc++.h>
 using namespace std;
